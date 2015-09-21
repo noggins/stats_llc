c09142b2f9a25bc8e5ab3ea36a1a8163b1f2a908
tigershen23 <tigershen23@gmail.com>
2015-09-21 09:14:52 -0700

    Scaffold a spec

--- spec/stats_api_spec.rb
+++ spec/stats_api_spec.rb
@@ -1,6 +1,11 @@
 require_relative './spec_helper'
 
 describe StatsAPI do
+  let(:api) { described_class.new(
+    api_key: ENV["STATS_API_KEY"],
+    secret: ENV["STATS_SECRET"]
+  ) }
+
   it 'has a version number' do
     expect(StatsAPI::VERSION).not_to be nil
   end
@@ -10,8 +15,10 @@ describe StatsAPI do
   end
 
   describe "#request" do
-    it 'prints poop' do
-      expect { StatsAPI.poop }.not_to raise_error
+    it "successfully sends a request to the Stats API and receives a response" do
+      url = '/decode/languages/'
+      response = api.request(url)
+      expect(response.status).to eq 200
     end
   end
 end
