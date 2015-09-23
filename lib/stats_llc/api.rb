972e3c0d0360ca811959d3a60163518f719be8a1
tigershen23 <tigershen23@gmail.com>
2015-09-23 08:49:05 -0700

    Add support for query string parameters

--- lib/stats_llc/api.rb
+++ lib/stats_llc/api.rb
@@ -13,22 +13,25 @@ module StatsLLC
       @accept_type = accept_type
     end
 
-    def get(path)
-      path_with_params = _build_path_with_params(path)
+    def get(path:, rest_params: {}, query_string_params: {})
+      path_with_params = _build_path_with_params(path, rest_params, query_string_params)
 
       self.class.get(path_with_params)
     end
 
-    def _build_path_with_params(path)
+    def _build_path_with_params(path, rest_params, query_string_params)
       sig = _generate_signature
-
       api_key_param = "?api_key=#{@api_key}"
       sig_param = "&sig=#{sig}"
 
       # TODO: clean up the generation of this path (e.g. join a params array with &)
-      path_with_params = "#{path}#{api_key_param}#{sig_param}"
+      path = "#{path}#{api_key_param}#{sig_param}"
 
-      path_with_params
+      path = _substitute_rest_params(path, rest_params)
+      path = _add_query_string_params(path, query_string_params)
+
+
+      path
     end
 
     def _generate_signature
@@ -36,9 +39,23 @@ module StatsLLC
       string_to_hash = "#{@api_key}#{@secret}#{now}"
       Digest::SHA256.new.update(string_to_hash).hexdigest
     end
-  end
 
-  class Response
+    def _substitute_rest_params(path, rest_params)
+      rest_params.each do |key, value|
+        param_to_interpolate = ":#{key}"
+        path.sub!(param_to_interpolate, value)
+      end
 
+      path
+    end
+
+    def _add_query_string_params(path, query_string_params)
+      query_string_params.each do |param|
+        #_add_query_string_param(path, param)
+        path += "&#{param[0]}=#{param[1]}"
+      end
+
+      path
+    end
   end
 end
--- spec/stats_llc/api_spec.rb
+++ spec/stats_llc/api_spec.rb
@@ -17,9 +17,41 @@ describe StatsLLC::API do
   describe "#get" do
     it "successfully sends a request to the Stats API and receives a response" do
       path = '/decode/languages'
-      response = api.get(path)
+      response = api.get(path: path)
       sleep(1)
       expect(response.code).to eq 200
+    end
+
+    it "successfully sends a request to the Stats API with rest parameters and receives a response" do
+      path = '/stats/basketball/nba/teams/:teamId'
+      response = api.get(
+        path: path,
+        rest_params: { teamId: '13' }
+      )
+      sleep(1)
+      json_response = JSON.parse(response, symbolize_names: true)
+
+      expect(response.code).to eq 200
+
+      season = json_response[:apiResults][0][:league][:season]
+      team_id = season[:conferences][0][:divisions][0][:teams][0][:teamId]
+      expect(team_id).to eq 13
+    end
+
+    it "successfully sends a request to the Stats API with query string parameters and receives a response" do
+      path = '/stats/basketball/nba/standings/'
+      response = api.get(
+        path: path,
+        query_string_params: { conference: true }
+      )
+      sleep(1)
+      json_response = JSON.parse(response, symbolize_names: true)
+
+      expect(response.code).to eq 200
+
+      season = json_response[:apiResults][0][:league][:season]
+      conferences = season[:eventType][0][:conferences]
+      expect(conferences.length).to eq 2
     end
   end
 end
